3bf734dffd6ee59b990db48690c0cf9e1057efa8
Marco Pattaro <mpattaro@gmail.com>
2013-08-02 16:28:38 +0200

    Add __unicode__ to Workflow

--- workflow/models.py
+++ workflow/models.py
@@ -6,3 +6,6 @@ from fields import WorkflowField
 class Workflow(models.Model):
     name = models.CharField(max_length=256, unique=True)
     workflow = WorkflowField()
+
+    def __unicode__(self):
+        return unicode(self.name)
