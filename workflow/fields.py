bd357496c2599a5fbc35e328c2c90dd273456d12
Marco Pattaro <mpattaro@gmail.com>
2013-08-02 17:50:55 +0200

    Fix to_python in order to accept string value on field update

--- workflow/fields.py
+++ workflow/fields.py
@@ -17,9 +17,15 @@ class WorkflowField(models.TextField):
             value = '{}'
         if isinstance(value, WF):
             return value
+        if isinstance(value, (str, unicode)):
+            return value.replace('null', 'None')
         return WF.parse(json.loads(value))
 
     def get_prep_value(self, value):
-        if not isinstance(value, WF):
-            raise TypeError("value not a Graph instance")
-        return json.dumps(value.__dict__)
+        if isinstance(value, WF):
+            ret = json.dumps(value.__dict__)
+        elif isinstance(value, (str, unicode)):
+            ret = json.dumps(eval(value))
+        else:
+            raise ValueError("value not a Graph instance")
+        return ret
